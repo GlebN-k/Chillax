029e28a1a66dd5eaa35f389147fa0b2a782a606d
nizhnikgleb <nizhnik.gleb@gmail.com>
2023-04-10 21:52:10 +0300

    changed homepage in package.json

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { store } from "./app/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <BrowserRouter basename="/Chillax">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </Provider>
